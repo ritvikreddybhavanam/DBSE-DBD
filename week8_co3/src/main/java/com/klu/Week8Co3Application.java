25c962054f48885fbf81fa78dc206220ee7794a9
ritvikreddybhavanam <ritvikreddybhavanamreddy@klh.edu.in>
2026-09-22 12:09:10 +0530

    commited files

---
+++ week8_co3/src/main/java/com/klu/Week8Co3Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8Co3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8Co3Application.class, args);
+    }
+
+}
---
+++ week8_co3/src/test/java/com/klu/Week8Co3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8Co3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
